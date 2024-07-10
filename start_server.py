e8b420ac52bf154a948cac2650c5c96394ec4518
hushengyong <hushengyong@yanxiu.com>
2024-07-10 19:27:47 +0800

    feat: host changed

--- start_server.py
+++ start_server.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     args = argp.parse_args()
     with open('logger.yaml') as fd:
         log_config = yaml.safe_load(fd)
-    run('main:app', host='127.0.0.1', port=args.port, reload=True, use_colors=True, log_config=log_config)
+    run('main:app', host='0.0.0.0', port=args.port, reload=True, use_colors=True, log_config=log_config)
